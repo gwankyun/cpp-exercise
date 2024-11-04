d9fea3a34df1ed662027e677ace9995e3d394cb9
權少 <ljqican@163.com>
2024-11-05 00:18:49 +0800

    添加hello-imgui示例。

---
+++ src/hello_imgui_example.cpp
@@ -0,0 +1,11 @@
+﻿#include <hello_imgui/hello_imgui.h>
+
+int main()
+{
+    HelloImGui::Run(
+        []()
+        {
+            ImGui::Text("Hello, world!");
+            ImGui::ShowDemoWindow();
+        });
+}
