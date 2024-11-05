a2999f318295a6359c8bc7a3be3244aed09f65a7
權少 <ljqican@163.com>
2024-11-05 23:24:40 +0800

    hello_imgui：添加按鍵示例。

--- src/hello_imgui_example.cpp
+++ src/hello_imgui_example.cpp
@@ -1,11 +1,18 @@
-﻿#include <hello_imgui/hello_imgui.h>
+﻿#include <string>
+
+#include <hello_imgui/hello_imgui.h>
 
 int main()
 {
+    std::string txt = "Hello, world!";
     HelloImGui::Run(
-        []()
+        [&]
         {
-            ImGui::Text("Hello, world!");
-            ImGui::ShowDemoWindow();
+            ImGui::Text(txt.c_str());
+            // ImGui::ShowDemoWindow();
+            if (ImGui::Button("click"))
+            {
+                txt = "click";
+            }
         });
 }
