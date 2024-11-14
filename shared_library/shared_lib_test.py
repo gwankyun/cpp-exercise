0b701649ffa92f72be2421c2e1aeaca30876f979
權少 <ljqican@163.com>
2024-11-15 00:02:08 +0800

    動態庫python測試改用unittest模塊。

--- shared_library/shared_lib_test.py
+++ shared_library/shared_lib_test.py
@@ -1,52 +1,102 @@
 import ctypes
-from ctypes import POINTER, Structure, c_int, c_double, c_long, byref
+from ctypes import POINTER, Structure, c_int, c_double, c_long, byref, c_char_p,\
+    c_size_t
 import pathlib
 import logging
+import unittest
 
 logging.basicConfig(
     level = logging.DEBUG,
     format = '[%(asctime)s] [%(levelname)s] [%(funcName)s:%(lineno)d] %(message)s'
     )
 
-current = pathlib.Path.cwd()
-
-logging.info(f'current: {current}')
-
-dll = ctypes.windll.LoadLibrary(current.joinpath('bin/shared_lib.dll'))
-
-if not dll:
-    logging.error(f'LoadLibrary bin/shared_lib.dll error')
-    exit(1)
-
-dll.add.argtypes = [c_long, c_long]
-dll.add.restype = c_long
-
-def add(a: int, b: int) -> int:
-    return dll.add(a, b)
-
-logging.info(f'add: {add(1, 2)}')
-
 class Point(Structure):
     _fields_ = [('x', c_int),
                 ('y', c_int)]
 
-# dll.Point_init.argtypes = [Point]
-pt = Point(1, 1)
+g_dll: ctypes.WinDLL = None
 
-logging.info(f'{pt.x} {pt.y}')
+class TestSharedLib(unittest.TestCase):
+    def get_dll(self):
+        return g_dll
 
-dll.Point_init(byref(pt))
+    def test_true(self):
+        self.assertTrue(True)
 
-logging.info(f'{pt.x} {pt.y}')
+    def test_add(self):
+        dll = self.get_dll()
+        dll.add.argtypes = [c_long, c_long]
+        dll.add.restype = c_long
 
-pt1 = Point(0, 0)
-pt2 = Point(3, 4)
+        def add(a: int, b: int) -> int:
+            return dll.add(a, b)
+        self.assertEqual(add(1, 2), 3)
+    
+    def test_Point_init(self):
+        dll = self.get_dll()
+        dll.Point_init.argtypes = [POINTER(Point)]
+        pt = Point(1, 1)
 
-dll.Point_distance.argtypes = [POINTER(Point), POINTER(Point)]
-dll.Point_distance.restype = c_double
+        dll.Point_init(byref(pt))
 
-distance = dll.Point_distance(byref(pt1), byref(pt2))
+        self.assertEqual((pt.x, pt.y), (0, 0))
 
-logging.info(f'distance: {distance}')
+    def test_Point_distance(self):
+        dll = self.get_dll()
+        pt1 = Point(0, 0)
+        pt2 = Point(3, 4)
 
-exit(0)
+        dll.Point_distance.argtypes = [POINTER(Point), POINTER(Point)]
+        dll.Point_distance.restype = c_double
+
+        distance = dll.Point_distance(byref(pt1), byref(pt2))
+        self.assertEqual(int(distance), 5)
+
+    def test_set_int(self):
+        dll = self.get_dll()
+        dll.set_int.argtypes = [POINTER(c_int), c_int]
+        value = c_int(0)
+        dll.set_int(byref(value), c_int(1))
+        self.assertEqual(value.value, 1)
+
+    def test_set_char_p(self):
+        dll = self.get_dll()
+        dll.set_char_p.argtypes = [POINTER(c_char_p), c_char_p]
+        c = c_char_p('123'.encode())
+        dll.set_char_p(byref(c), c_char_p('abc'.encode()))
+        self.assertEqual(c.value.decode(), 'abc')
+
+    def test_set_struct(self):
+        dll = self.get_dll()
+        dll.set_struct.argtypes = [POINTER(Point), c_int, c_int]
+        pt = Point(1, 2)
+        dll.set_struct(byref(pt), 3, 4)
+        self.assertEqual((pt.x, pt.y), (3, 4))
+
+    def test_set_array(self):
+        dll = self.get_dll()
+        dll.set_array.argtypes = [POINTER(c_int), c_size_t, c_int]
+        c_int_array_type = c_int * 10
+        a = c_int_array_type()
+        dll.set_array(a, c_size_t(3), 4)
+        self.assertEqual(a[3], 4)
+
+if __name__ == '__main__':
+    if g_dll == None:
+        current = pathlib.Path.cwd()
+
+        logging.info(f'current: {current}')
+
+        if current.name == 'bin':
+            current = current.parent
+
+        dll_file = current.joinpath('bin/shared_lib.dll')
+        if not dll_file.exists():
+            logging.error(f'{dll_file} not exists')
+            exit(1)
+
+        g_dll = ctypes.windll.LoadLibrary(dll_file)
+
+    unittest.main()
+
+    exit(0)
--- shared_library/src/shared_lib_test.cpp
+++ shared_library/src/shared_lib_test.cpp
@@ -16,39 +16,41 @@ namespace fs = std::filesystem;
 TEST_CASE("void* and any", "[ptr]")
 {
     auto dll_path = fs::current_path() / "shared_lib.dll";
-    REQUIRE(fs::exists(dll_path));
-    if (!fs::exists(dll_path))
-    {
-        return;
-    }
+    CHECK(fs::exists(dll_path));
 
     dylib lib(dll_path);
 
     namespace sl = shared_lib;
 
     auto add = lib.get_function<decltype(sl::add)>("add");
-
+    CHECK(add != nullptr);
     REQUIRE(add(1, 2) == 3);
 
-    sl::Point pt;
     auto Point_init = lib.get_function<decltype(sl::Point_init)>("Point_init");
-    Point_init(&pt);
-    REQUIRE((pt.x == 0 && pt.y == 0));
-
-    sl::Point pt1;
-    Point_init(&pt1);
-
-    sl::Point pt2;
-    Point_init(&pt2);
-    pt2.x = 3;
-    pt2.y = 4;
+    CHECK(Point_init != nullptr);
+    {
+        sl::Point pt;
+        Point_init(&pt);
+        REQUIRE((pt.x == 0 && pt.y == 0));
+    }
 
     auto Point_distance = lib.get_function<decltype(sl::Point_distance)>("Point_distance");
+    CHECK(Point_distance != nullptr);
+    {
+        sl::Point pt1;
+        Point_init(&pt1);
 
-    auto dist = Point_distance(&pt1, &pt2);
-    REQUIRE(static_cast<int>(dist) == 5);
+        sl::Point pt2;
+        Point_init(&pt2);
+        pt2.x = 3;
+        pt2.y = 4;
+
+        auto dist = Point_distance(&pt1, &pt2);
+        REQUIRE(static_cast<int>(dist) == 5);
+    }
 
     auto set_int = lib.get_function<decltype(sl::set_int)>("set_int");
+    CHECK(set_int != nullptr);
     {
         int v = 0;
         set_int(&v, 1);
@@ -56,6 +58,7 @@ TEST_CASE("void* and any", "[ptr]")
     }
 
     auto set_char_p = lib.get_function<decltype(sl::set_char_p)>("set_char_p");
+    CHECK(set_char_p != nullptr);
     {
         char* str = nullptr;
         set_char_p(&str, "123");
@@ -63,6 +66,7 @@ TEST_CASE("void* and any", "[ptr]")
     }
 
     auto set_struct = lib.get_function<decltype(sl::set_struct)>("set_struct");
+    CHECK(set_struct != nullptr);
     {
         sl::Point pt;
         set_struct(&pt, 3, 4);
@@ -70,6 +74,7 @@ TEST_CASE("void* and any", "[ptr]")
     }
 
     auto set_array = lib.get_function<decltype(sl::set_array)>("set_array");
+    CHECK(set_array != nullptr);
     {
         int a[10];
         set_array(a, 3, 4);
