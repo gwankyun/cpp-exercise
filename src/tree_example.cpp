42eb4f1be2c1c025877aed4f154eb38e10669eec
權少 <ljqican@163.com>
2025-04-04 10:56:30 +0800

    樹數據結構練習。

--- src/tree_example.cpp
+++ src/tree_example.cpp
@@ -1,44 +1,136 @@
 ﻿#include <string>
+#include <vector>
+#include <cassert>
 
 #include <spdlog/spdlog.h>
 
-struct TreeNode
+
+namespace tree
 {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int _x, TreeNode* _left = nullptr, TreeNode* _right = nullptr) : val(_x), left(_left), right(_right)
+    struct Node
     {
-    }
-    ~TreeNode()
+        int val;
+        Node* left;
+        Node* right;
+        Node(int _x, Node* _left = nullptr, Node* _right = nullptr) : val(_x), left(_left), right(_right)
+        {
+        }
+        ~Node()
+        {
+            if (left != nullptr)
+            {
+                delete left;
+                left = nullptr;
+            }
+            if (right != nullptr)
+            {
+                delete right;
+                right = nullptr;
+            }
+        }
+    };
+
+    bool is_leaf(const Node* _node)
     {
-        if (left != nullptr)
+        assert(_node != nullptr);
+        return _node->left == nullptr && _node->right == nullptr;
+    }
+
+    int degree(const Node* _node)
+    {
+        assert(_node!= nullptr);
+        return int(_node->left != nullptr) + int(_node->right != nullptr);
+    }
+
+    enum struct Side
+    {
+        Left,
+        Right
+    };
+
+    Node* left(Node* _node)
+    {
+        if (_node == nullptr)
         {
-            delete left;
-            left = nullptr;
+            return nullptr;
         }
-        if (right != nullptr)
+        return _node->left;
+    }
+
+    Node* right(Node* _node)
+    {
+        if (_node == nullptr)
         {
-            delete right;
-            right = nullptr;
+            return nullptr;
+        }
+        return _node->right;
+    }
+
+    void insert(Node* _node, Side _side, Node* _new_node)
+    {
+        assert(_node != nullptr);
+        assert(_new_node != nullptr);
+        auto& node = *_node;
+        auto& new_node = *_new_node;
+
+        if (_side == Side::Left)
+        {
+            new_node.left = node.left;
+            node.left = &new_node;
+        }
+        else if (_side == Side::Right)
+        {
+            new_node.right = node.right;
+            node.right = &new_node;
         }
     }
-};
+
+    Node* remove(Node* _node, Side _side)
+    {
+        assert(_node != nullptr);
+        auto& node = *_node;
+
+        if (_side == Side::Left)
+        {
+            auto tmp = node.left;
+            node.left = left(tmp);
+            return tmp;
+        }
+        else if (_side == Side::Right)
+        {
+            auto tmp = node.right;
+            node.right = right(tmp);
+            return tmp;
+        }
+    }
+} // namespace tree
 
 void init_tree()
 {
-    auto tree = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+    std::vector<tree::Node*> node(10, nullptr);
+
+    for (auto i = 1; i < 6; i++)
+    {
+        node[i] = new tree::Node(i);
+    }
+
+    node[1]->left = node[2];
+    node[1]->right = node[3];
+    node[2]->left = node[4];
+    node[2]->right = node[5];
+
+    auto tree = node[1];
 
     // 插入新節點
-    auto p = new TreeNode(0);
-    auto n1 = tree;
-    auto n2 = n1->left;
-    n1->left = p;
-    p->left = n2;
+    tree::insert(node[1], tree::Side::Left, new tree::Node(0));
 
     // 刪除節點
-    n1->left = n2;
-    delete p;
+    auto p = tree::remove(node[1], tree::Side::Left);
+    if (p != nullptr)
+    {
+        delete p;
+        p = nullptr;
+    }
 
     delete tree;
 }
